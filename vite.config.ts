ed924dfd588a6b91fb9959e495822a1c98a2f527
Akshayaa139 <akshlearn13@gmail.com>
2025-12-19 22:09:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      '.onrender.com'
-    ]
-  }
+  host: true,
+  allowedHosts: 'all'
+}
+
 })
 
